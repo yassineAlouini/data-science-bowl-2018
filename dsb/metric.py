abe187bf5510d6fcfdc58b86b2f85a1f7d510c9e
YassineAlouini <yassinealouini@outlook.com>
2018-04-08 14:37:32 +0200

    Start adding the IoU metric. WIP

---
+++ dsb/metric.py
@@ -0,0 +1,8 @@
+# "Special" IoU metric implementation
+# TODO: Add documentation and link to definition
+
+
+def dsb_iou():
+    """ A specific IoU (intersection over union) for the dsb competition
+    """
+    pass
