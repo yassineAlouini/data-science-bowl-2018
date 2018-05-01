c6d7603b1e50b8f50605cd87cafe12e5d244c718
YassineAlouini <yassinealouini@outlook.com>
2018-05-01 16:55:37 +0200

    Tests work now

--- dsb/metric.py
+++ dsb/metric.py
@@ -16,13 +16,12 @@ N_STEPS = int((END - START) / STEP) + 2
 THRESHOLDS = np.linspace(START, END, N_STEPS)
 
 
-def _custom_precision(labels, total_true, total_pred, positive_class=1):
+def _custom_precision(labels_matrix, total_true, total_pred, positive_class=1):
     """ A custom precision (i.e. different to the common one) metric for a two
     classes (positive and negative) classification.
     Notice that this custom precision is exactly the Jaccard similarity coefficient.
     """
-    true_positive = (labels == positive_class).sum()
-    print(true_positive)
+    true_positive = (labels_matrix == positive_class).sum(axis=0)
     false_positive = total_pred - true_positive
     false_negative = total_true - true_positive
     return true_positive / (true_positive + false_positive + false_negative)
@@ -47,31 +46,33 @@ def _labeling(pred_mask, true_mask, thresholds):
     return (computed_iou > thresholds).astype(int)
 
 
-def dsb_metric(pred_masks, true_masks, thresolds=THRESHOLDS):
+def dsb_metric(pred_masks, true_masks, thresholds=THRESHOLDS):
     """ A specific IoU (intersection over union) for the dsb competition
     """
     total_true_masks = len(pred_masks)
     total_pred_masks = len(true_masks)
-    mean_jacard_sim_coeff = 0.0
-    # TODO: Something is off, fix it!
-    for pred_mask, true_mask in zip(pred_masks, true_masks):
-        # One label per threshold
-        labels = _labeling(pred_mask, true_mask, thresolds)
-        jacard_sim_coeff = _custom_precision(labels, total_true_masks, total_pred_masks).mean()
-        print(jacard_sim_coeff)
-        mean_jacard_sim_coeff += jacard_sim_coeff
-    return mean_jacard_sim_coeff / total_true_masks
+    labels = [_labeling(pred_mask, true_mask, thresholds) for pred_mask, true_mask in zip(pred_masks, true_masks)]
+
+    labels_matrix = np.matrix(labels)
+    assert labels_matrix.shape == (total_true_masks, len(thresholds))  #  (Number of masks, Number of thresholds)
+    jacard_sim_coeff = _custom_precision(labels_matrix, total_true_masks, total_pred_masks)
+    assert jacard_sim_coeff.shape == (1, len(thresholds))  #  (1, Number of thresholds)
+    return jacard_sim_coeff.mean()
 
 
 def _test_dsb_metric():
     """ Check that the metric works as expected: load masks for an image and
-    then compute the score comparing these masks with themselves.
+    then compute the score. For the the original masks, the score should be 1.0. For "background" masks
+    (predict a very small value everywhere, 1 for example), the score should be 0.0.
     """
     masks = []
     for mask_path in glob.iglob(TEST_MASK_PATH):
         mask = cv2.imread(mask_path)
         masks.append(mask)
-    print(dsb_metric(masks, masks))
+    background_mask = np.ones(mask.shape, dtype=np.int)
+    background_masks = [background_mask for _ in range(len(masks))]
+    assert dsb_metric(masks, masks) == 1.0
+    assert dsb_metric(masks, background_masks) == 0.0
 
 
 if __name__ == '__main__':
